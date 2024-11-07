dd4f3fe0f45a8f4c9e6c71b81b2da15b3bfa77fc
Amithabh-A <amithabh2002@gmail.com>
2024-11-08 01:55:29 +0530

    Updated animations and scripts

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 const GRAVITY = 3000
 const SPEED = 400
 const JUMPSPEED = 1000
+var animatedPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(5)
-	pass # Replace with function body.
+	animatedPlayer = $"AnimatedSprite2D"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,15 +15,23 @@ func _process(delta):
 	
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta 
+		animatedPlayer.play("jump")
+	else:
+		animatedPlayer.play("idle")
 
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("Left"):
+		animatedPlayer.play("run")
+		animatedPlayer.scale.x = -1
 		velocity.x = -SPEED
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("Right"):
+		animatedPlayer.play("run")
+		animatedPlayer.scale.x = 1
 		velocity.x = SPEED
 	else:
 		velocity.x = 0
+		
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Up") and is_on_floor():
 		velocity.y -= JUMPSPEED
 		
 	move_and_slide()

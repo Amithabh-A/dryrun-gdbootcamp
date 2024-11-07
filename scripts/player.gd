837335349b416fc9259c062683b26e64d929606b
Amithabh-A <amithabh2002@gmail.com>
2024-11-07 11:52:20 +0530

    Initial Animations, flip, sprint(not needed)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,29 +1,79 @@
 extends CharacterBody2D
 
-const GRAVITY = 3000
-const SPEED = 400
-const JUMPSPEED = 1000
+const GRAVITY = 15
+const SPEED = 10
+const SPRINT = 3000
+const HIT_AND_JUMP = 600
+const JUMPSPEED = 500
+var sprinting = false
+var jump_offset = Vector2(10, 0)
+var run_offset = Vector2(10, 0)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(5)
-	pass # Replace with function body.
-
+	idle()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	idle()
+	vertical_motion()
+	horizontal_motion()
+	move_and_slide()
 	
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta 
+func idle():
+	$"AnimatedSprite2D".play("Idle")
+	
+func jump(reverse = false):
+	if reverse:
+		$AnimatedSprite2D.scale.x = -1
+		position -= jump_offset
+	else:
+		$AnimatedSprite2D.scale.x = 1
+		position += jump_offset
+	$AnimatedSprite2D.play("Jump")
+	
+func run(reverse = false):
+	
+	if reverse:
+		$AnimatedSprite2D.scale.x = -1
+		position -= run_offset
+	else:
+		$AnimatedSprite2D.scale.x = 1
+		position += run_offset
+	$AnimatedSprite2D.play("Run")
 
-	if Input.is_action_pressed("ui_left"):
+func horizontal_motion():
+	if Input.is_action_pressed("Left"):
 		velocity.x = -SPEED
-	elif Input.is_action_pressed("ui_right"):
+		print(velocity.x)
+		print("left")
+		if is_on_floor():
+			run(true)
+		else:
+			jump(true)
+	elif Input.is_action_pressed("Right"):
 		velocity.x = SPEED
+		print(velocity.x)
+		print("right")
+		if is_on_floor():
+			run()
+		else:
+			jump()
 	else:
 		velocity.x = 0
+		idle()
+
+func vertical_motion():
+	velocity.y += 10
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y -= JUMPSPEED
+	if Input.is_action_just_pressed("Down") and not is_on_floor():
+		velocity.y = SPRINT
+		sprinting = true
 		
-	move_and_slide()
+	if is_on_floor() and sprinting:
+		velocity.y = -HIT_AND_JUMP
+		sprinting = false
+	
+	if Input.is_action_just_pressed("Up") and is_on_floor():
+		velocity.y = -JUMPSPEED
